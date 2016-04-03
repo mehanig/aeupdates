40ef2fe717030668bff0f2a83302230ef559f0b9
mehanig <mehanig@gmail.com>
2016-04-04 01:42:19 +0300

    docker and nginx fixes 2

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+UPLOADS_DIR=/aeupdates
+
+set -e
+# Create folders required by nginx upload module
+mkdir -p $UPLOADS_DIR/nginx_folder/upload/tmp/{0..9}
+chmod -R 777 $UPLOADS_DIR/nginx_folder
 
 /usr/local/nginx/sbin/nginx -c /aeupdates/nginx_conf/nginx.conf
