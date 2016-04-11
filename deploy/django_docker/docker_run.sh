c5fbe4d3cfa216362b8b61d0f885e6642e54992d
mehanig <mehanig@gmail.com>
2016-04-11 20:48:49 +0300

    deploy with docker improvements

--- deploy/django_docker/docker_run.sh
+++ deploy/django_docker/docker_run.sh
@@ -4,7 +4,7 @@ UPLOADS_DIR=/aeupdates
 
 set -e
 # Create folders required by nginx upload module
-mkdir -p $UPLOADS_DIR/nginx_folder/upload/tmp/{0..9}
-chmod -R 777 $UPLOADS_DIR/nginx_folder
+mkdir -p $UPLOADS_DIR/docker_nginx_folder/upload/tmp/{0..9}
+chmod -R 777 $UPLOADS_DIR/docker_nginx_folder
 
 /usr/local/nginx/sbin/nginx -c /aeupdates/nginx_conf/nginx.conf
