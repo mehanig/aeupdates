6425433d6893ee39237939fbbad3251b2e986034
mehanig <mehanig@gmail.com>
2016-04-15 02:10:08 +0300

    django deployment on uwsgi

--- aeupdates/settings.py
+++ aeupdates/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-from aeupdates.config import MY_SECRET_KEY
+from config import MY_SECRET_KEY
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
--- deploy/django_docker/docker_run.sh
+++ deploy/django_docker/docker_run.sh
@@ -8,4 +8,6 @@ mkdir -p $UPLOADS_DIR/docker_nginx_folder/upload/tmp/{0..9}
 chmod -R 777 $UPLOADS_DIR/docker_nginx_folder
 
 /usr/local/nginx/sbin/nginx -c /aeupdates/nginx_conf/nginx.conf
+
+source /aeupdates_venv/bin/activate && cd /aeupdates && uwsgi --socket aeupdates.sock --module aeupdates.wsgi --chmod-socket=666
 tail -f /dev/null
