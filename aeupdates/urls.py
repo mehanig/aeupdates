3584d2045611545eddc54a679fb13bb59a016b69
mehanig <mehanig@gmail.com>
2016-04-15 02:55:33 +0300

    include admin url

--- aeupdates/urls.py
+++ aeupdates/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls import url, include, patterns
 from rest_framework import routers
 from apps.subscriptions import views
+from django.contrib import admin
 from django.contrib.staticfiles.views import serve
 from .views import MainPageView
 
@@ -12,6 +13,7 @@ router.register(r'groups', views.GroupViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
