4aac5337d2ccda56cb4e3ee6b58cc6be558aaca6
mehanig <mehanig@gmail.com>
2016-04-16 23:10:11 +0300

    back to python3.4 bacause of psycopg2, damn

--- aeupdates/settings.py
+++ aeupdates/settings.py
@@ -77,13 +77,25 @@ WSGI_APPLICATION = 'aeupdates.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+IS_PRODUCTION = False
+if IS_PRODUCTION:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'aeupdates',
+            'USER': 'aeupdates_user',
+            'PASSWORD': '',
+            'HOST': '',
+            'PORT': 'db_port_number',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Internationalization
