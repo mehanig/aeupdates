93d7060199b15e6430a556819cb0de31a002a663
mehanig <mehanig@gmail.com>
2016-04-21 02:32:56 +0300

    schema start

---
+++ apps/frontend/app/models/news-item-compiled.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  ver: DS.attr('string'),
+  content: DS.attr('string')
+});
+
+//# sourceMappingURL=news-item-compiled.js.map
---
+++ apps/frontend/app/models/script-compiled.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  url: DS.attr('string'),
+  product: DS.attr('string'),
+  version: DS.attr('string'),
+  news: DS.hasMany('news-item')
+});
+
+//# sourceMappingURL=script-compiled.js.map
