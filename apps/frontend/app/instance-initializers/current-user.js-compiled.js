92680cf232c9bf2d40b26902876cb3e32527f5e0
mehanig <mehanig@gmail.com>
2016-04-22 23:28:30 +0300

    initializers

---
+++ apps/frontend/app/instance-initializers/current-user.js-compiled.js
@@ -0,0 +1,10 @@
+export function initialize() /* appInstance */{
+  // appInstance.inject('route', 'foo', 'service:foo');
+}
+
+export default {
+  name: 'current-user.js',
+  initialize
+};
+
+//# sourceMappingURL=current-user.js-compiled.js.map
