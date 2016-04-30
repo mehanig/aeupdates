1b7e3ecf7d7441afc63eb2ab9b8d475874258204
mehanig <mehanig@gmail.com>
2016-04-30 22:57:04 +0300

    change build

--- deploy/django_docker/docker_run.sh
+++ deploy/django_docker/docker_run.sh
@@ -7,6 +7,10 @@ set -e
 mkdir -p $UPLOADS_DIR/docker_nginx_folder/upload/tmp/{0..9}
 chmod -R 777 $UPLOADS_DIR/docker_nginx_folder
 
+# Build Ember Project
+
+cd /aeupdates/apps/frontend && ember build -o /aeupdates/aeupdates/static/ember
+
 /usr/local/nginx/sbin/nginx -c /aeupdates/nginx_conf/nginx.conf
 
 source /aeupdates_venv/bin/activate && cd /aeupdates && uwsgi --socket aeupdates.sock --module aeupdates.wsgi --chmod-socket=666
