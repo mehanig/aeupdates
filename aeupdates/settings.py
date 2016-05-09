1293304524bb29bd9a601501a5f23c384abecd30
mehanig <mehanig@gmail.com>
2016-05-09 20:29:10 +0300

    configs

--- aeupdates/settings.py
+++ aeupdates/settings.py
@@ -12,7 +12,11 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-from secret_config import *
+
+try:
+    from secret_config import *
+except ImportError:
+    from public_config import *
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
---
+++ public_config.py
@@ -0,0 +1,5 @@
+MY_SECRET_KEY = '_mexqeci3@6kpf()czw^*^szel&-bgm-fr=dl@g8!3_y*$2!lg'
+MY_PSQL_USER = "psql"
+MY_PSQL_PASS = "psqlpsqlpsql"
+MY_EMAIL_HOST_USER = "user@aeupdates.com"
+MY_EMAIL_HOST_PASSWORD = "passpasspasspass"
