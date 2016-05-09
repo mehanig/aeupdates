7fc69b17e690e505faaa3f372cbd7fd1735b0f7f
mehanig <mehanig@gmail.com>
2016-05-09 22:35:01 +0300

    change public config

--- public_config.py
+++ public_config.py
@@ -1,5 +1,7 @@
 MY_SECRET_KEY = '_mexqeci3@6kpf()czw^*^szel&-bgm-fr=dl@g8!3_y*$2!lg'
-MY_PSQL_USER = "psql"
-MY_PSQL_PASS = "psqlpsqlpsql"
+MY_PSQL_USER = "postgres"
+MY_PSQL_PASS = "mysecretpassword"
 MY_EMAIL_HOST_USER = "user@aeupdates.com"
 MY_EMAIL_HOST_PASSWORD = "passpasspasspass"
+
+MY_CONFIG_IS_PRODUCTION = True
