77bb56b26f3c6f7f480ea6a6802a69ec6372fc75
mehanig <mehanig@gmail.com>
2016-05-11 03:16:09 +0300

    fix Ember Store to sent sessions in headers

--- apps/frontend/app/adapters/application.js
+++ apps/frontend/app/adapters/application.js
@@ -1,7 +1,14 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 import DRFAdapter from './drf';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import ENV from 'frontend/config/environment';
+
+$.ajaxSetup({
+  xhrFields: {
+    withCredentials: true
+  }
+});
 
 export default DRFAdapter.extend({
   addTrailingSlashes: false
@@ -17,6 +24,11 @@ export default DS.RESTAdapter.extend({
   buildURL: function(type, id, record) {
     //call the default buildURL and then append a slash
     return this._super(type, id, record) + '/';
+  },
+  ajax: function(url, method, hash) {
+    hash = hash || {};
+    hash.xhrFields = {withCredentials: true};
+    return this._super(url, method, hash);
   }
 });
 
