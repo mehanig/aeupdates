9caab15d434365eab9db51f8e1f1dcd04ec08f8a
mehanig <mehanig@gmail.com>
2016-07-11 23:31:39 +0300

    bad hack(

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,8 +1,9 @@
+import codecs
+import json
+
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
-from collections import OrderedDict
-from rest_framework.renderers import JSONRenderer
 from rest_framework_json_api.renderers import JSONRenderer
 
 from apps.products.models import Product
@@ -10,11 +11,24 @@ from apps.news.models import News
 from apps.products.serializers import ProductSerializer
 
 
+# TODO: Response from server now is pure JSON, not DRF web page with data :(
+# TODO: ATTENTION THIS IS SUPER BAD HACK!
+
 class SortedRelationsRenderer(JSONRenderer):
+    """
+    Custom JSONRenderer class, used to sort relationships by custom field.
+    Can't being achieved in other way, because DjangoJSONAPI library sorts results
+    """
     def render(self, data, accepted_media_type=None, renderer_context=None):
-        data = data
         obj = super(SortedRelationsRenderer, self).render(data, accepted_media_type, renderer_context)
-        return obj
+        obj = json.loads(obj.decode('utf-8'))
+        obj['data']['relationships']['news']['data'] = sorted(obj['data']['relationships']['news']['data'],
+                                                              key=lambda el: News.objects.get(id=el['id']).version, reverse=True)
+        obj = json.dumps(
+            obj, cls=self.encoder_class, ensure_ascii=self.ensure_ascii,
+        )
+        obj = obj.replace('\u2028', '\\u2028').replace('\u2029', '\\u2029')
+        return bytes(obj.encode('utf-8'))
 
 
 class ProductViewSet(viewsets.ModelViewSet):
