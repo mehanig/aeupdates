438c6c2baf95b387c24e52f731d3b6e9bfbb6fbd
mehanig <mehanig@gmail.com>
2016-09-18 20:13:36 +0300

    clean

--- apps/frontend/config/staging_environment.js
+++ apps/frontend/config/staging_environment.js
@@ -42,34 +42,15 @@ module.exports = function(environment) {
   if (environment === 'development') {
     ENV.APP.API_HOST = 'http://localhost:8000';
     ENV.APP.API_NAMESPACE = 'api';
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
   if (environment === 'test') {
-    // Testem prefers this...
-
-    // ENV.baseURL = '/';
-    // ENV.locationType = 'none';
-    // ENV.APP.API_HOST = 'http://37.139.30.9:8080';
-    // ENV.APP.API_NAMESPACE = 'api';
-    //
-    // keep test console output quieter
-    // ENV.APP.LOG_ACTIVE_GENERATION = false;
-    // ENV.APP.LOG_VIEW_LOOKUPS = false;
-    //
-    // ENV.APP.rootElement = '#ember-testing';
   }
 
   if (environment === 'production') {
     ENV.APP.API_HOST  = 'http://37.139.30.9:8080';
     ENV.APP.API_NAMESPACE = 'api';
 
-    // UNCOMMENT FOR LOCAL DEVELOPMENT IF PRODUCTION BUILD NEEDED
-    // ENV.APP.API_HOST = 'http://37.139.30.9:8080';
 }
   return ENV;
 };
